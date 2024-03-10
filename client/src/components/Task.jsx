7fe8d5e116007d2a02f714f644aab77eb2ebb6ba
Mark <ajineer@outlook.com>
2024-03-10 12:34:00 -0600

    added rest of crud operations to task componenet, next is to nail down the styling.

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -1,12 +1,24 @@
-import { Button, ListItem } from "@mui/material"
-import { deleteTaskReq } from "../routes/taskRoutes"
+import { Box, Button, Checkbox, ListItem, TextField, Typography } from "@mui/material"
+import { deleteTaskReq, patchTaskReq } from "../routes/taskRoutes"
 import { useAuthContext } from "../hooks/useAuthContext"
 import { useListContext } from "../hooks/useListContext"
+import { useEffect, useState } from "react"
 
 const Task = ({ setSelList, selList, task}) => {
     
     const {user} = useAuthContext()
     const {lists, dispatch} = useListContext()
+    const [edit, setEdit] = useState(false)
+    const [editTaskForm, setEditTaskForm] = useState({
+        name: '',
+        status: false
+    })
+    const list = lists.filter(l => l._id === task.list)
+
+    useEffect(() => {
+        setEditTaskForm({...task})
+    },[selList, task])
+
     const handleDelete = async (e) => {
         const response = await deleteTaskReq(task, user)
         const json = await response.json()
@@ -16,10 +28,35 @@ const Task = ({ setSelList, selList, task}) => {
         }
     }
 
+    const handleEdit = async (e) => {
+        
+        e.preventDefault()
+        const response = await patchTaskReq(e, task, user)
+        const json = await response.json()
+        list.tasks?.map(t => {
+            return t._id === task._id ? json : t
+        })
+        if(response.ok){
+            setEditTaskForm({...json})
+            dispatch({type: 'UPDATE_LIST', payload: list})
+            setEdit(false)
+        }
+    }
+
     return (
 
         <ListItem>
-            {task.name}<Button onClick={(e) => handleDelete(e)}>X</Button>
+           {edit?<form onSubmit={(e) => handleEdit(e)}>
+                <TextField required name='name' placeholder={editTaskForm.name} type="text" onChange={(e) => setEditTaskForm({name: e.target.value})}></TextField>
+            </form>:
+            <Box>
+                <Typography variant="span">
+                    {editTaskForm.name}
+                </Typography>
+                <Button onClick={(e) => handleDelete(e)}>X</Button>
+                <Button onClick={() => setEdit(true)}>/</Button>
+                <Checkbox checked={editTaskForm.status} onChange={(e) => handleEdit(e)}/>
+            </Box>}
         </ListItem>
     )
 }
