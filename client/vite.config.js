6d85fb262a6eccf85821b933ae179906a948b462
Mark <ajineer@outlook.com>
2024-03-15 15:32:12 -0600

    added deployed server url to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     cors: true,
     proxy: {
       "/api": {
-        target: "http://localhost:5555",
+        target: "https://mern-task-master-server.onrender.com",
         secure: false,
       },
     },
